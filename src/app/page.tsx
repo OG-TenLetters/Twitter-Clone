f45d3e8ceca5d7558f541c1f1e7ac631028d6371
Jadon <1tenletters0@gmail.com>
2025-03-31 09:31:43 -0700

    The sidebar is done, Timeline feed is next.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,101 @@
-import Image from "next/image";
+import Link from "next/link";
+import { BiHomeCircle, BiUser } from "react-icons/bi";
+import {
+  BsBell,
+  BsBookmark,
+  BsEnvelope,
+  BsThreeDots,
+  BsTwitter,
+} from "react-icons/bs";
+import { HiOutlineHashtag } from "react-icons/hi";
 
-export default function Home() {
+const NAVIGATION_ITEMS = [
+  {
+    title: "Twitter",
+    icon: BsTwitter,
+  },
+  {
+    title: "Home",
+    icon: BiHomeCircle,
+  },
+  {
+    title: "Explore",
+    icon: HiOutlineHashtag,
+  },
+  {
+    title: "Notifications",
+    icon: BsBell,
+  },
+  {
+    title: "Messages",
+    icon: BsEnvelope,
+  },
+  {
+    title: "Bookmarks",
+    icon: BsBookmark,
+  },
+  {
+    title: "Profile",
+    icon: BiUser,
+  },
+];
+
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="w-full h-full flex justify-center items-center 
+    relative bg-black"
+    >
+      <div className="max-w-screen-xl w-full h-full flex relative">
+        {/* left sidebar for navigation/header */}
+        <section
+          className="fixed w-[275px] flex flex-col items-stretch 
+        h-screen"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <div className="flex flex-col items-stretch h-full space-y-4 
+          mt-4">
+            {NAVIGATION_ITEMS.map((item) => (
+              <Link
+                className="hover:bg-white/10 transition duration-200 
+                text-2xl flex items-center justify-start w-fit space-x-2 
+                rounded-3xl py-2 px-6"
+                href={`${item.title.toLowerCase()}`}
+                key={item.title}
+              >
+                <div>
+                  <item.icon />
+                </div>
+                {item.title !== "Twitter" && <div>{item.title}</div>}
+              </Link>
+            ))}
+            <button className="rounded-full bg-birdie m-4 p-4 
+            text-2xl text-center hover:bg-birdie/70 transition 
+            duration-200">
+              Tweet
+            </button>
+          </div>
+          <button className="rounded-full flex items-center space-x-2 m-4 
+          bg-transparent p-4 text-center hover:bg-white/20 transition 
+          duration-200 w-full justify-between">
+            <div className="flex items-center space-x-2">
+              <div className="rounded-full bg-slate-400 w-12 h-12"></div>
+              <div className="text-left">
+                <div className="font-semibold text-sm"> Jadon Smith</div>
+                <div className="text-xs">@TenLetters</div>
+              </div>
+            </div>
+              <div>
+                <BsThreeDots />
+              </div>
+          </button>
+        </section>
+        {/* Home */}
+        <main></main>
+        {/* Right Section */}
+        <section></section>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
